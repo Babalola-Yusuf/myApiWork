f56618474b1f7c3f2eac34f7e1ebcc29b4ace8d4
Babalola-Yusuf <105571942+Babalola-Yusuf@users.noreply.github.com>
2023-02-08 12:45:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,138 @@
-fetch('https://jsonplaceholder.typicode.com/users') .then(response => response.json()) .then(json =>{
+
+    fetch('https://jsonplaceholder.typicode.com/users') .then(response => response.json()) .then(json =>{
+
     console.log(json);
-    displayData(json);
+
+return   displayData(json);
+
 } 
+
     )
+
     
+
     let i = 0;// initializing the index
-    
+
+     
+
+    let picChange = () =>{
+
+        document.getElementById("robo").src =`https://robohash.org/${i}`;
+
+    } 
+
+     
+
+     // previous button   
+
     prev=()=>{
-    while (i>0) {
+
+    fetch('https://jsonplaceholder.typicode.com/users') .then(response => response.json()) .then(json =>{
+
+    console.log(json);
+
+return   displayData(json);
+
+    
+
+} 
+
+    )
+
+    if (i>0) {
+
         i--;
-       return displayData();
-    }/*else {
+
+        picChange();
+
+       displayData(json);
+
+       
+
+    }else {
+
         alert ('Thats the end!');
-    }*/
+
+    }
+
 }
+
+//next button
 
 next =()=>{
-    while (i<5) {
+
+fetch('https://jsonplaceholder.typicode.com/users') .then(response => response.json()) .then(json =>{
+
+    console.log(json);
+
+  displayData(json);
+
+        
+
+} 
+
+    )
+
+    if (i<9) {
+
         i++;
-      return displayData();
-    }/*else {
-        alert('Thats the end!')
-    }*/
-    
-}
+
+        picChange();
+
+       displayData();
+
+    }else {
+
+    alert('Thats the end!')
+
+    }
 
     
-    // a function to display the converted json from the api
+
+    }
+
     
-    displayData =(json) =>{      
+
+    // a function to display the converted json from the api on the html page
+
+    
+
+    displayData =(json) =>{     
+
+     
+
     var obj = json[i];
-    /*name*/
-      const name = document.getElementById('name');
-      const apiName = obj.name;
-     name.innerHTML= apiName;
-   // username  
-     const username = document.getElementById('uname');
-     const apiUsername =obj.username;
-     username.innerHTML = apiUsername;
-     // id
-     const id = document.getElementById('id');
-     const apiId =obj.id;
-        id.innerHTML = apiId;
-        //city
-    const city = document.getElementById('city');
-     const apiCity =obj.address.city;
-        city.innerHTML = apiCity;
-        }
 
+    /*name*/
+
+      const name = document.getElementById('name');
+
+      const apiName = obj.name;
+
+     name.innerHTML= apiName;
+
+   // username  
+
+     const username = document.getElementById('uname');
+
+     const apiUsername =obj.username;
+
+     username.innerHTML = apiUsername;
+
+     // id
+
+     const id = document.getElementById('id');
+
+     const apiId =obj.id;
+
+        id.innerHTML = apiId;
+
+        //city
+
+    const city = document.getElementById('city');
+
+     const apiCity =obj.address.city;
+
+        city.innerHTML = apiCity;
+
+        }
